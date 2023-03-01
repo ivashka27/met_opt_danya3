2c8a6e1a80106a74bf606f0103b0e03b7e2b1152
Danil Zasukhin <awishyhere@gmail.com>
2023-03-01 16:51:55 +0300

    3d plotter

--- lab1/method/gradient_descent.py
+++ lab1/method/gradient_descent.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 
-def gradient_descent(f, grad, lr=0.2, epoch=30, start=3):
-    x = np.array([start])
+def gradient_descent(f, grad, n, start, lr=0.2, epoch=30):
+    x = np.array(start)
 
-    points = np.zeros(epoch)
+    points = np.zeros((epoch, n))
     points[0] = x
     for i in range(1, epoch):
         x = x - lr * grad(x)
--- lab1/plot/plotter.py
+++ lab1/plot/plotter.py
@@ -11,3 +11,23 @@ def points_over_function(points, f):
     ax2.plot(points, f(points), 'o-')
     print(points[-1], f(points[-1]))
     plt.show()
+
+
+def three_dim_plot(f):
+    t = np.linspace(-1.5, 1.5, 100)
+    X, Y = np.meshgrid(t, t)
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.plot_surface(X, Y, f(np.stack((X, Y))))
+    plt.show()
+
+
+def points_over_contour(points, f):
+    t = np.linspace(-1.5, 1.5, 100)
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.plot(f(points.T))
+    ax1.grid()
+    X, Y = np.meshgrid(t, t)
+    ax2.plot(points[:, 0], points[:, 1], 'o-')
+    ax2.contour(X, Y, f(np.stack((X, Y))), levels=np.sort(np.concatenate((f(points.T), np.linspace(-1, 1, 100)))))
+    print(points[-1], f(points[-1]))
+    plt.show()
