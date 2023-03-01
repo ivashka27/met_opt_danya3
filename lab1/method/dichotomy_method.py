561b0b50d3965a14d5f4baf5cdaa31a29478e38a
chipch1n <cripshook3@gmail.com>
2023-03-01 16:27:36 +0300

    dichotomy_method implemented

---
+++ lab1/method/dichotomy_method.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def dichotomy_method(f, a, b):
+    epoch = 30
+    eps = 0.2
+    points = np.zeros(epoch)
+
+    for i in range(epoch):
+        x = (a + b) / 2
+        f1 = f(x - eps)
+        f2 = f(x + eps)
+        if f1 < f2:
+            b = x
+        else:
+            a = x
+        points[i] = x
+    return points
