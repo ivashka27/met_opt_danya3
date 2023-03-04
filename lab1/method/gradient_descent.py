fixes and dichotomy gradient method

lab1/method/gradient_descent.py
@@ -1,12 +1,15 @@
 import numpy as np
 
 
-def gradient_descent(f, grad, n, start, lr=0.2, epoch=30):
+def gradient_descent(f, grad, start, eps=0.01, lr=0.2, max_iter=10000):
     x = np.array(start)
 
-    points = np.zeros((epoch, n))
-    points[0] = x
-    for i in range(1, epoch):
+    points = [x]
+    i = 1
+    while i < max_iter:
         x = x - lr * grad(x)
-        points[i] = x
-    return points
+        points.append(x)
+        if abs(f(points[i]) - f(points[i - 1])) <= eps:
+            break
+        i += 1
+    return np.asarray(points)

lab1/plot/plotter.py
@@ -9,25 +9,24 @@ def points_over_function(points, f):
     ax1.plot(f(points))
     ax2.plot(t, f(t))
     ax2.plot(points, f(points), 'o-')
-    print(points[-1], f(points[-1]))
     plt.show()
 
 
-def three_dim_plot(f):
-    t = np.linspace(-1.5, 1.5, 100)
+def three_dim_plot(f, sz=2):
+    t = np.linspace(-sz, sz, 100)
     X, Y = np.meshgrid(t, t)
     ax = plt.figure().add_subplot(projection='3d')
     ax.plot_surface(X, Y, f(np.stack((X, Y))))
     plt.show()
 
 
-def points_over_contour(points, f):
-    t = np.linspace(-1.5, 1.5, 100)
+def points_over_contour(points, f, levels=30):
+    a = max(-np.min(points), np.max(points)) + 0.1
+    t = np.linspace(-a, a, 100)
+    X, Y = np.meshgrid(t, t)
     fig, (ax1, ax2) = plt.subplots(1, 2)
     ax1.plot(f(points.T))
     ax1.grid()
-    X, Y = np.meshgrid(t, t)
+    ax2.contour(X, Y, f(np.stack((X, Y))), levels=levels)
     ax2.plot(points[:, 0], points[:, 1], 'o-')
-    ax2.contour(X, Y, f(np.stack((X, Y))), levels=np.sort(np.concatenate((f(points.T), np.linspace(-1, 1, 100)))))
-    print(points[-1], f(points[-1]))
     plt.show()