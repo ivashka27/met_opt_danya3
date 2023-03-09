9012dadcb096255892ea88bb5f1facdb3be566ea
Danil Zasukhin <awishyhere@gmail.com>
2023-03-09 22:11:58 +0300

    generated function at main and number of calculations

--- lab1/main.py
+++ lab1/main.py
@@ -2,37 +2,51 @@ import numpy as np
 import lab1.method.gradient_descent as gd
 import lab1.method.dichotomy_gradient as dg
 import lab1.plot.plotter as plotter
+import lab1.method.func_generation as fg
+
+# def f(x):
+#     return 10 * x[0] ** 2 + x[1] ** 2
+
+N = 2
+
+matrix = fg.generate_func(N, 20)
+
+
+def multiply(x, m, n):
+    v = np.zeros(n, dtype=object)
+    for i in range(n):
+        for j in range(n):
+            v[i] += x[j] * m[j][i]
+    res = 0
+    for i in range(n):
+        res += v[i] * x[i]
+    return res
 
 
 def f(x):
-    return 10 * (x[1] - x[0]) ** 2 + (1 - x[0]) ** 2
+    n = N
+    return multiply(x, matrix, n)
 
 
 def grad(x):
     h = 1e-5
-    return (f(x[:, np.newaxis] + h * np.eye(2)) - f(x[:, np.newaxis] - h * np.eye(2))) / (2 * h)
+    n = N
+    return (f(x[:, np.newaxis] + h * np.eye(n)) - f(x[:, np.newaxis] - h * np.eye(n))) / (2 * h)
 
 
-#start = 10 * np.random.rand(1, 2)[0]
-start = [1, -2]
-print(start)
-points = gd.gradient_descent(f, grad, start, 1e-6, 0.01, 100000)
-# print(f(points.T))
+start = 10 * np.random.rand(1, N)[0]
+
+print("start:", start)
+points = gd.gradient_descent(f, grad, start, 1e-6, 0.04, 100000)
+
 plotter.three_dim_plot(f, 100)
 pl1 = plotter.points_over_contour(points, f)
 pl1.title("gradient descent")
 pl1.show()
-print(len(points))
 print(points[-1], f(points[-1]))
 
-points2 = dg.dichotomy_gradient(f, grad, start, 1e-6, 1e-5, 200)
+points2 = dg.dichotomy_gradient(f, grad, start, 1e-6, 1e-3, 1)
 pl2 = plotter.points_over_contour(points2, f)
 pl2.title("dichotomy descent")
 pl2.show()
-print(len(points2))
 print(points2[-1], f(points2[-1]))
-
-# points3 = fg.fibonacci_gradient(f, grad, [-0.3, 1], 1e-5, lr=0.01)
-# plotter.points_over_contour(points3, f).show()
-# print(len(points3))
-# print(points3[-1], f(points3[-1]))
--- lab1/method/dichotomy_gradient.py
+++ lab1/method/dichotomy_gradient.py
@@ -5,20 +5,24 @@ def dichotomy_gradient(f, grad, start, eps_f=0.01, eps_r=0.001, lr=0.2, max_iter
     x = np.array(start)
     points = [x]
 
+    cnt = 0
     for i in range(max_iter):
         a = 0
         b = lr
         d = grad(x)
         while abs(b - a) > 2 * eps_r:
             c = (a + b) / 2
+            cnt += 2
             if f(x - ((c + eps_r) * d)) < f(x - ((c - eps_r) * d)):
                 a = c
             else:
                 b = c
 
-        #print((a + b) / 2.0)
         x = x - ((a + b) / 2.0) * d
         points.append(x)
         if abs(f(points[i]) - f(points[i - 1])) <= eps_f:
             break
+    print("dichotomy gradient:")
+    print("gradient:", len(points))
+    print("fun_cnt:", cnt)
     return np.asarray(points)
