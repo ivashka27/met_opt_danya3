c0ba1ba0edab03dbe36af160e518310dd31828d7
Danil Zasukhin <awishyhere@gmail.com>
2023-04-06 14:59:03 +0300

    task 3

---
+++ lab2/main.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import methods
+
+np.random.seed(1212)
+X = np.random.rand(200, 1)
+y = 0 + 10 * X + np.random.randn(200, 1)
+
+learning_rate = 0.3
+n_epochs = 100
+batch_sizes = [1, 10, 20, 50]
+
+print("SGD:")
+plt.figure(figsize=(10, 7))
+for i, batch_size in enumerate(batch_sizes):
+    w = methods.stochastic_gradient_descent(X, y, learning_rate=learning_rate, n_epochs=n_epochs, batch_size=batch_size)
+    print("batch: {}. w: {}".format(batch_size, w))
+    plt.plot(X, y, 'b.')
+    plt.plot(X, X.dot(w), 'r-', label=f'Batch size {batch_size}')
+plt.legend()
+plt.title('sgd')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+print("Momentum:")
+for i, batch_size in enumerate(batch_sizes):
+    w = methods.sgd_with_momentum(X, y, learning_rate=learning_rate, n_epochs=n_epochs, batch_size=batch_size)
+    print("batch: {}. w: {}".format(batch_size, w))
+    plt.plot(X, y, 'b.')
+    plt.plot(X, X.dot(w), 'r-', label=f'Batch size {batch_size}')
+plt.legend()
+plt.title('momentum')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+print("Nesterov:")
+for i, batch_size in enumerate(batch_sizes):
+    w = methods.sgd_nesterov(X, y, learning_rate=learning_rate, n_epochs=n_epochs, batch_size=batch_size)
+    print("batch: {}. w: {}".format(batch_size, w))
+    plt.plot(X, y, 'b.')
+    plt.plot(X, X.dot(w), 'r-', label=f'Batch size {batch_size}')
+plt.legend()
+plt.title('nesterov')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+print("AdaGrad:")
+for i, batch_size in enumerate(batch_sizes):
+    w = methods.sgd_adagrad(X, y, learning_rate=learning_rate, n_epochs=n_epochs, batch_size=batch_size)
+    print("batch: {}. w: {}".format(batch_size, w))
+    plt.plot(X, y, 'b.')
+    plt.plot(X, X.dot(w), 'r-', label=f'Batch size {batch_size}')
+plt.legend()
+plt.title('AdaGrad')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+print("RMSprop:")
+for i, batch_size in enumerate(batch_sizes):
+    w = methods.sgd_rmsprop(X, y, learning_rate=learning_rate, n_epochs=n_epochs, batch_size=batch_size)
+    print("batch: {}. w: {}".format(batch_size, w))
+    plt.plot(X, y, 'b.')
+    plt.plot(X, X.dot(w), 'r-', label=f'Batch size {batch_size}')
+plt.legend()
+plt.title('RMSprop')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
+
+
+print("Adam:")
+for i, batch_size in enumerate(batch_sizes):
+    w = methods.sgd_adam(X, y, learning_rate=learning_rate, n_epochs=n_epochs, batch_size=batch_size)
+    print("batch: {}. w: {}".format(batch_size, w))
+    plt.plot(X, y, 'b.')
+    plt.plot(X, X.dot(w), 'r-', label=f'Batch size {batch_size}')
+plt.legend()
+plt.title('Adam')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
--- lab2/methods.py
+++ lab2/methods.py
@@ -1,9 +1,53 @@
+import math
+
 import numpy as np
 
 
-def stochastic_gradient_descent(x, y, learning_rate=0.1, n_epochs=50, batch_size=1):
+def gradient_descent_with_direction(x, y, function, learning_rate=0.1, n_epochs=50, batch_size=1):
     n_samples, n_features = x.shape
     w = np.random.randn(n_features, 1)
+    for epoch in range(n_epochs):
+        indices = np.random.permutation(n_samples)
+        x_shuffled = x[indices]
+        y_shuffled = y[indices]
+        prev_v = 0
+        for i in range(0, n_samples, batch_size):
+            x_i = x_shuffled[i:i + batch_size]
+            y_i = y_shuffled[i:i + batch_size]
+            cur_v = function(x_i, y_i, w, prev_v)
+            w = w - learning_rate * cur_v
+            prev_v = cur_v
+    return w
+
+
+def gradient(x_i, y_i, w, batch_size):
+    return x_i.T.dot(x_i.dot(w) - y_i) / batch_size
+
+
+def stochastic_gradient_descent(x, y, learning_rate=0.1, n_epochs=50, batch_size=1):
+    return gradient_descent_with_direction(x, y, lambda x_i, y_i, w, prev_v: gradient(x_i, y_i, w, batch_size),
+                                           learning_rate, n_epochs, batch_size)
+
+
+def sgd_with_momentum(x, y, gamma=0.9, learning_rate=0.1, n_epochs=50, batch_size=1):
+    return gradient_descent_with_direction(x, y,
+                                           lambda x_i, y_i, w, prev_v:
+                                           gamma * prev_v + (1 - gamma) * gradient(x_i, y_i, w, batch_size),
+                                           learning_rate, n_epochs, batch_size)
+
+
+def sgd_nesterov(x, y, gamma=0.9, learning_rate=0.1, n_epochs=50, batch_size=1):
+    return gradient_descent_with_direction(x, y,
+                                           lambda x_i, y_i, w, prev_v:
+                                           gamma * prev_v + (1 - gamma) * gradient(
+                                               x_i, y_i, w - learning_rate * gamma * prev_v, batch_size),
+                                           learning_rate, n_epochs, batch_size)
+
+
+def sgd_adagrad(x, y, learning_rate=0.1, n_epochs=50, batch_size=1):
+    n_samples, n_features = x.shape
+    w = np.random.randn(n_features, 1)
+    gr_sum = 0
     for epoch in range(n_epochs):
         indices = np.random.permutation(n_samples)
         x_shuffled = x[indices]
@@ -11,6 +55,52 @@ def stochastic_gradient_descent(x, y, learning_rate=0.1, n_epochs=50, batch_size
         for i in range(0, n_samples, batch_size):
             x_i = x_shuffled[i:i + batch_size]
             y_i = y_shuffled[i:i + batch_size]
-            gradient = x_i.T.dot(x_i.dot(w) - y_i) / batch_size
-            w = w - learning_rate * gradient
+            gr = gradient(x_i, y_i, w, batch_size)
+            gr_sum += np.diag(gr).dot(np.diag(gr))
+            w = w - learning_rate * gr / math.sqrt(gr_sum)
+    return w
+
+
+def sgd_rmsprop(x, y, beta=0.99, eps=1e-6, learning_rate=0.1, n_epochs=50, batch_size=1):
+    n_samples, n_features = x.shape
+    w = np.random.randn(n_features, 1)
+    prev_s = 0
+    for epoch in range(n_epochs):
+        indices = np.random.permutation(n_samples)
+        x_shuffled = x[indices]
+        y_shuffled = y[indices]
+        for i in range(0, n_samples, batch_size):
+            x_i = x_shuffled[i:i + batch_size]
+            y_i = y_shuffled[i:i + batch_size]
+            gr = gradient(x_i, y_i, w, batch_size)
+            s = beta * prev_s + (1 - beta) * (gr ** 2)
+            w = w - learning_rate * gr / np.sqrt(s + eps)
+            prev_s = s
+    return w
+
+
+def sgd_adam(x, y, beta1=0.9, beta2=0.99, eps=1e-6, learning_rate=0.1, n_epochs=50, batch_size=1):
+    n_samples, n_features = x.shape
+    w = np.random.randn(n_features, 1)
+    prev_v = 0
+    prev_s = 0
+    beta1_pw = beta1
+    beta2_pw = beta2
+    for epoch in range(n_epochs):
+        indices = np.random.permutation(n_samples)
+        x_shuffled = x[indices]
+        y_shuffled = y[indices]
+        for i in range(0, n_samples, batch_size):
+            x_i = x_shuffled[i:i + batch_size]
+            y_i = y_shuffled[i:i + batch_size]
+            gr = gradient(x_i, y_i, w, batch_size)
+            v = beta1 * prev_v + (1 - beta1) * gr
+            s = beta2 * prev_s + (1 - beta2) * (gr ** 2)
+            v_sm = v / (1 - beta1_pw)
+            s_sm = s / (1 - beta2_pw)
+            w = w - learning_rate * v_sm / np.sqrt(s_sm + eps)
+            beta1_pw *= beta1
+            beta2_pw *= beta2
+            prev_v = v
+            prev_s = s
     return w
