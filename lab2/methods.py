b7629435c7f38708d2e055bff804524b3c7c7837
Danil Zasukhin <awishyhere@gmail.com>
2023-04-10 01:48:49 +0300

    task 3

--- lab2/methods.py
+++ lab2/methods.py
@@ -7,7 +7,6 @@ exp_learning_rate = lambda epoch: 0.5 * exp(-0.1 * epoch)
 step_learning_rate = lambda epoch: 0.1 if epoch < 50 else 0.01
 
 
-
 def gradient_descent_with_direction(x, y, function, learning_rate=lambda epoch: 0.1, n_epochs=50, batch_size=1):
     n_samples, n_features = x.shape
     w = np.random.randn(n_features, 1)
@@ -34,78 +33,90 @@ def stochastic_gradient_descent(x, y, learning_rate=const_learning_rate, n_epoch
                                            learning_rate, n_epochs, batch_size)
 
 
-def sgd_with_momentum(x, y, gamma=0.9, learning_rate=const_learning_rate, n_epochs=50, batch_size=1):
-    return gradient_descent_with_direction(x, y,
-                                           lambda x_i, y_i, w, prev_v, epoch:
-                                           gamma * prev_v + (1 - gamma) * gradient(x_i, y_i, w),
-                                           learning_rate, n_epochs, batch_size)
+def sgd_with_momentum(f, grad, start, gamma=0.9, eps_g=1e-6, learning_rate=const_learning_rate, max_iter=10000):
+    x = np.array(start)
+
+    points = [x]
+    prev_v = 0
+    for epoch in range(max_iter):
+        gr = grad(x)
+        v = gamma * prev_v + (1 - gamma) * gr
+        prev_v = v
+        x = x - learning_rate(epoch) * v
+        points.append(x)
+        if np.linalg.norm(gr) < eps_g:
+            break
+    return np.asarray(points), len(points), 0
 
 
-def sgd_nesterov(x, y, gamma=0.9, learning_rate=const_learning_rate, n_epochs=50, batch_size=1):
-    return gradient_descent_with_direction(x, y,
-                                           lambda x_i, y_i, w, prev_v, epoch:
-                                           gamma * prev_v + (1 - gamma) * gradient(
-                                               x_i, y_i, w - learning_rate(epoch) * gamma * prev_v),
-                                           learning_rate, n_epochs, batch_size)
+def sgd_nesterov(f, grad, start, gamma=0.9, eps_g=1e-6, learning_rate=const_learning_rate, max_iter=10000):
+    x = np.array(start)
+
+    points = [x]
+    prev_v = 0
+    for epoch in range(max_iter):
+        gr = grad(x - learning_rate(epoch) * gamma * prev_v)
+        v = gamma * prev_v + (1 - gamma) * gr
+        prev_v = v
+        x = x - learning_rate(epoch) * v
+        points.append(x)
+        if np.linalg.norm(gr) < eps_g:
+            break
+    return np.asarray(points), len(points), 0
 
 
-def sgd_adagrad(x, y, learning_rate=const_learning_rate, n_epochs=50, batch_size=1):
-    n_samples, n_features = x.shape
-    w = np.random.randn(n_features, 1)
+def sgd_adagrad(f, grad, start, eps_g=1e-6, learning_rate=const_learning_rate, max_iter=10000):
+    x = np.array(start)
+
+    points = [x]
     gr_sum = 0
-    for epoch in range(n_epochs):
-        indices = np.random.permutation(n_samples)
-        x_shuffled = x[indices]
-        y_shuffled = y[indices]
-        for i in range(0, n_samples, batch_size):
-            x_i = x_shuffled[i:i + batch_size]
-            y_i = y_shuffled[i:i + batch_size]
-            gr = gradient(x_i, y_i, w)
-            gr_sum += np.diag(gr).dot(np.diag(gr))
-            w = w - learning_rate(epoch) * gr / math.sqrt(gr_sum)
-    return w
+    for epoch in range(max_iter):
+        gr = grad(x)
+        gr_sum += np.diag(gr).dot(np.diag(gr))
+        x = x - learning_rate(epoch) * gr / math.sqrt(gr_sum)
+        points.append(x)
+        if np.linalg.norm(gr) < eps_g:
+            break
+    return np.asarray(points), len(points), 0
 
 
-def sgd_rmsprop(x, y, beta=0.99, eps=1e-8, learning_rate=const_learning_rate, n_epochs=50, batch_size=1):
-    n_samples, n_features = x.shape
-    w = np.random.randn(n_features, 1)
+def sgd_rmsprop(f, grad, start, beta=0.99, eps_f=1e-8, eps_g=1e-6, learning_rate=const_learning_rate, max_iter=10000):
+    x = np.array(start)
+
+    points = [x]
     prev_s = 0
-    for epoch in range(n_epochs):
-        indices = np.random.permutation(n_samples)
-        x_shuffled = x[indices]
-        y_shuffled = y[indices]
-        for i in range(0, n_samples, batch_size):
-            x_i = x_shuffled[i:i + batch_size]
-            y_i = y_shuffled[i:i + batch_size]
-            gr = gradient(x_i, y_i, w)
-            s = beta * prev_s + (1 - beta) * (gr ** 2)
-            w = w - learning_rate(epoch) * gr / np.sqrt(s + eps)
-            prev_s = s
-    return w
+    for epoch in range(max_iter):
+        gr = grad(x)
+        s = beta * prev_s + (1 - beta) * (gr ** 2)
+        x = x - learning_rate(epoch) * gr / np.sqrt(s + eps_f)
+        points.append(x)
+        prev_s = s
+        if np.linalg.norm(gr) < eps_g:
+            break
+    return np.asarray(points), len(points), 0
 
 
-def sgd_adam(x, y, beta1=0.9, beta2=0.99, eps=1e-8, learning_rate=const_learning_rate, n_epochs=50, batch_size=1):
-    n_samples, n_features = x.shape
-    w = np.random.randn(n_features, 1)
+def sgd_adam(f, grad, start, beta1=0.9, beta2=0.99, eps_f=1e-8, eps_g=1e-6, learning_rate=const_learning_rate,
+             max_iter=10000):
+    x = np.array(start)
+
+    points = [x]
     prev_v = 0
     prev_s = 0
     beta1_pw = beta1
     beta2_pw = beta2
-    for epoch in range(n_epochs):
-        indices = np.random.permutation(n_samples)
-        x_shuffled = x[indices]
-        y_shuffled = y[indices]
-        for i in range(0, n_samples, batch_size):
-            x_i = x_shuffled[i:i + batch_size]
-            y_i = y_shuffled[i:i + batch_size]
-            gr = gradient(x_i, y_i, w)
-            v = beta1 * prev_v + (1 - beta1) * gr
-            s = beta2 * prev_s + (1 - beta2) * (gr ** 2)
-            v_sm = v / (1 - beta1_pw)
-            s_sm = s / (1 - beta2_pw)
-            w = w - learning_rate(epoch) * v_sm / np.sqrt(s_sm + eps)
-            beta1_pw *= beta1
-            beta2_pw *= beta2
-            prev_v = v
-            prev_s = s
-    return w
+    for epoch in range(max_iter):
+        gr = grad(x)
+        v = beta1 * prev_v + (1 - beta1) * gr
+        s = beta2 * prev_s + (1 - beta2) * (gr ** 2)
+        v_sm = v / (1 - beta1_pw)
+        s_sm = s / (1 - beta2_pw)
+        x = x - learning_rate(epoch) * v_sm / np.sqrt(s_sm + eps_f)
+        points.append(x)
+        beta1_pw *= beta1
+        beta2_pw *= beta2
+        prev_v = v
+        prev_s = s
+        if np.linalg.norm(gr) < eps_g:
+            break
+    return np.asarray(points), len(points), 0
