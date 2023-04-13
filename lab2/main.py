3e29883ca4199172cb0fc89b79ae7e338779af02
Danil Zasukhin <awishyhere@gmail.com>
2023-04-13 22:03:30 +0300

    colorized batch

--- lab2/main.py
+++ lab2/main.py
@@ -2,21 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import methods
 
+n=100
+dim=1
 np.random.seed(1212)
-X = np.random.rand(200, 1)
-y = 0 + 10 * X + np.random.randn(200, 1)
+X = np.random.rand(n, dim)
+y = 5 * X + np.random.randn(n, dim)
 
 learning_rate = methods.const_learning_rate
-n_epochs = 100
-batch_sizes = [1, 10, 20, 50]
+n_epochs = 150
+batch_sizes = [1, 10, 20, 50, 100]
 
 print("SGD:")
 plt.figure(figsize=(10, 7))
+colors = ['r', 'b', 'y', 'g', 'purple']
 for i, batch_size in enumerate(batch_sizes):
     w = methods.stochastic_gradient_descent(X, y, learning_rate=learning_rate, n_epochs=n_epochs, batch_size=batch_size)
     print("batch: {}. w: {}".format(batch_size, w))
     plt.plot(X, y, 'b.')
-    plt.plot(X, X.dot(w), 'r-', label=f'Batch size {batch_size}')
+    plt.plot(X, X.dot(w), '-', color=colors[i], label=f'Batch size {batch_size}', alpha=0.7)
 plt.legend()
 plt.title('sgd')
 plt.xlabel('x')
