7fa6e6918d0875b0dd290853a023ab3fd1e026e3
Danil Zasukhin <awishyhere@gmail.com>
2023-04-14 20:35:10 +0300

    batch research fixed

--- lab2/main.py
+++ lab2/main.py
@@ -6,17 +6,16 @@ n = 300
 dim = 1
 np.random.seed(1212)
 X = 5 * np.random.rand(n, dim)
-y = 4 * X + 1 * np.random.randn(n, dim)
+y = 6 * X + 1 * np.random.randn(n, dim)
 
 learning_rate = methods.const_learning_rate
-n_epochs = 150
 batch_sizes = [1, 50, 100, 200, 300]
 
 print("SGD:")
 colors = ['r', 'orange', 'y', 'g', 'violet']
 for i, batch_size in enumerate(batch_sizes):
-    w = methods.stochastic_gradient_descent(X, y, learning_rate=learning_rate, n_epochs=n_epochs, batch_size=batch_size)
-    print("batch: {}. w: {}".format(batch_size, w))
+    w, iters = methods.stochastic_gradient_descent(X, y, learning_rate=learning_rate, batch_size=batch_size)
+    print("batch: {}. w: {}. iters: {}".format(batch_size, w, iters))
     plt.plot(X, y, 'b.')
     plt.plot(X, X.dot(w), '-', color=colors[i], label=f'Batch size {batch_size}', linewidth=3)
     plt.legend()
@@ -24,3 +23,4 @@ for i, batch_size in enumerate(batch_sizes):
     plt.xlabel('x')
     plt.ylabel('y')
     plt.show()
+
--- lab2/methods.py
+++ lab2/methods.py
@@ -7,30 +7,39 @@ exp_learning_rate = lambda epoch: 0.5 * exp(-0.1 * epoch)
 step_learning_rate = lambda epoch: 0.1 if epoch < 50 else 0.01
 
 
-def gradient_descent_with_direction(x, y, function, learning_rate=lambda epoch: 0.1, n_epochs=50, batch_size=1):
+def gradient_descent_with_direction(x, y, function, learning_rate=lambda epoch: 0.1, eps=1e-6, batch_size=1,
+                                    max_iter=10000):
     n_samples, n_features = x.shape
     w = np.random.randn(n_features, 1)
-    for epoch in range(n_epochs):
+    iter = 0
+    for epoch in range(max_iter):
+        iter += 1
         indices = np.random.permutation(n_samples)
         x_shuffled = x[indices]
         y_shuffled = y[indices]
         prev_v = 0
+        sum_v = 0
+        cnt = 0
         for i in range(0, n_samples, batch_size):
             x_i = x_shuffled[i:i + batch_size]
             y_i = y_shuffled[i:i + batch_size]
             cur_v = function(x_i, y_i, w, prev_v, epoch)
             w = w - learning_rate(epoch) * cur_v
             prev_v = cur_v
-    return w
+            sum_v += abs(prev_v)
+            cnt += 1
+        if abs(sum_v / cnt) < eps:
+            break
+    return w, iter
 
 
 def gradient(x_i, y_i, w):
     return x_i.T.dot(x_i.dot(w) - y_i) / len(x_i)
 
 
-def stochastic_gradient_descent(x, y, learning_rate=const_learning_rate, n_epochs=50, batch_size=1):
+def stochastic_gradient_descent(x, y, learning_rate=const_learning_rate, eps=1e-6, batch_size=1, max_iter=10000):
     return gradient_descent_with_direction(x, y, lambda x_i, y_i, w, prev_v, epoch: gradient(x_i, y_i, w),
-                                           learning_rate, n_epochs, batch_size)
+                                           learning_rate, eps, batch_size, max_iter)
 
 
 def sgd_with_momentum(f, grad, start, gamma=0.9, eps_g=1e-6, learning_rate=const_learning_rate, max_iter=10000,
@@ -84,7 +93,7 @@ def sgd_adagrad(f, grad, start, eps_g=1e-6, learning_rate=const_learning_rate, m
         for i in range(0, len(gr)):
             gr_sum += gr[i] * gr.T[i]
         d = gr / math.sqrt(gr_sum)
-        x = x - learning_rate(epoch) *d
+        x = x - learning_rate(epoch) * d
         if trajectory:
             points.append(x)
         if np.linalg.norm(d) < eps_g:
